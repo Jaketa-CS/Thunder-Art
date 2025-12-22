ffcfcfdcb9eb8dfa87448f90d02d2bbfd1b36849
Jake Teeter <Jteeteraco@gmail.com>
2025-12-21 18:31:16 -0700

    navbar touchups

--- thunder-web/src/components/Navbar.tsx
+++ thunder-web/src/components/Navbar.tsx
@@ -27,85 +27,109 @@ const Navbar = () => {
       <Link
         to="/"
         style={{
-          fontSize: '2.25rem',
-          fontWeight: '800',
-          fontFamily: 'var(--font-family-header)',
           display: 'flex',
+          flexDirection: 'column',
           alignItems: 'center',
-          gap: '4px',
+          gap: '0px',
+          marginTop: '-15px',
         }}
       >
-        THUNDERFENNEC
-        <span
-          style={{
-            color: 'var(--color-accent)',
-            display: 'inline-flex',
-            alignItems: 'center',
-            marginLeft: '2px',
-            position: 'relative',
-          }}
-        >
-          {/* Primary Bolt */}
-          <svg
-            width="0.8em"
-            height="0.8em"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{
-              transform: 'translateY(-2px) rotate(15deg) scale(0.8, 1.5)',
-              animation: 'electric-glitch 5s infinite ease-in-out',
-            }}
-          >
-            <defs>
-              <linearGradient id="bolt-grad" x1="0" y1="0" x2="1" y2="1">
-                <stop offset="0%" stopColor="#00E5FF" />
-                <stop offset="100%" stopColor="#2979FF" />
-              </linearGradient>
-            </defs>
-            <path
-              d="M13 2L3 14H12L11 22L21 10H12L13 2Z"
-              fill="url(#bolt-grad)"
-            />
-          </svg>
-
-          {/* Secondary Bolt - appears during glitch */}
-          <svg
-            width="0.8em"
-            height="0.8em"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            style={{
-              position: 'absolute',
-              left: 0,
-              top: 0,
-              transform: 'translate(3px, -4px) rotate(20deg) scale(0.9, 1.2)',
-              animation:
-                'electric-glitch-2 5s infinite ease-in-out, bolt-appear 5s infinite',
-              pointerEvents: 'none',
-            }}
-          >
-            <defs>
-              <linearGradient id="bolt-grad-2" x1="0" y1="0" x2="1" y2="1">
-                <stop offset="0%" stopColor="#00FFFF" />
-                <stop offset="100%" stopColor="#0080FF" />
-              </linearGradient>
-            </defs>
-            <path
-              d="M13 2L3 14H12L11 22L21 10H12L13 2Z"
-              fill="url(#bolt-grad-2)"
-            />
-          </svg>
-        </span>
         <img
           src="/dance.gif"
           alt="Dancing Character"
-          style={{ height: '60px', width: 'auto' }} // Bigger
+          style={{
+            height: '120px',
+            width: 'auto',
+            marginRight: '30px',
+          }}
         />
+        <div
+          style={{
+            fontSize: '1.7rem',
+            fontWeight: '700',
+            fontFamily: 'var(--font-family-header)',
+            display: 'flex',
+            alignItems: 'center',
+            marginTop: '-20px',
+            letterSpacing: '0.15rem',
+          }}
+        >
+          THUNDER
+          <span
+            style={{
+              color: 'var(--color-accent)',
+              display: 'inline-flex',
+              alignItems: 'center',
+              marginLeft: '2px',
+              position: 'relative',
+            }}
+          >
+            {/* Primary Bolt */}
+            <svg
+              width="0.8em"
+              height="0.8em"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              style={{
+                transform: 'translateY(-2px) rotate(15deg) scale(0.8, 1.5)',
+                animation: 'electric-glitch 5s infinite ease-in-out',
+              }}
+            >
+              <defs>
+                <linearGradient id="bolt-grad" x1="0" y1="0" x2="1" y2="1">
+                  <stop offset="0%" stopColor="#00E5FF" />
+                  <stop offset="100%" stopColor="#2979FF" />
+                </linearGradient>
+              </defs>
+              <path
+                d="M13 2L3 14H12L11 22L21 10H12L13 2Z"
+                fill="url(#bolt-grad)"
+              />
+            </svg>
+
+            {/* Secondary Bolt - appears during glitch */}
+            <svg
+              width="0.8em"
+              height="0.8em"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              style={{
+                position: 'absolute',
+                left: 0,
+                top: 0,
+                transform: 'translate(3px, -4px) rotate(20deg) scale(0.7, 1.4)',
+                animation:
+                  'electric-glitch-2 5s infinite ease-in-out, bolt-appear 5s infinite',
+                pointerEvents: 'none',
+              }}
+            >
+              <defs>
+                <linearGradient id="bolt-grad-2" x1="0" y1="0" x2="1" y2="1">
+                  <stop offset="0%" stopColor="#00FFFF" />
+                  <stop offset="100%" stopColor="#0080FF" />
+                </linearGradient>
+              </defs>
+              <path
+                d="M13 2L3 14H12L11 22L21 10H12L13 2Z"
+                fill="url(#bolt-grad-2)"
+              />
+            </svg>
+          </span>
+        </div>
       </Link>
 
-      <div style={{ display: 'flex', gap: '2rem', alignItems: 'center' }}>
+      <div
+        style={{
+          display: 'flex',
+          gap: '2rem',
+          alignItems: 'center',
+          marginBottom: '50px', // Adjusted down slightly
+          fontSize: '1.1rem', // Bigger menu text
+          fontWeight: '500',
+        }}
+      >
         <NavLink to="/" active={isActive('/')}>
           Work
         </NavLink>
